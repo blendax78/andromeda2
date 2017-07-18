6994e31259e28ea9e18bc98b7d9cc2a39d51dc50
Eric Vintimilla <eric@vintimilla.org>
2017-07-18 17:52:57 -0400

    add index & config

--- app/config.py
+++ app/config.py
@@ -1,7 +1,52 @@
+import os
+# import logging
+
 class Config(object):
 
-  def log(self):
-    print('hello')
+  def __init__(self):
+    self.db = {
+      'db': 'andromeda'
+    }
+    self.logfile = '/home/blendax/www/log/python/andromeda2.log'
+    self.configfile = '/home/blendax/www/lib/digitalocean.ini'
+
+  def log(self, subject, message):
+    print(subject, message)
+
     return True
 
+  def set_db_config():
+    config = get_config()
 
+    db = config[self.db['db']]
+    self.db = db
+    return db
+
+  def get_config():
+    config = None
+
+    if os.path.isfile(log_file):
+     config = configparser.ConfigParser()
+     config.read(self.configfile)
+
+    return config
+
+# def get_connection():
+#   db = get_db_config()
+#   # Connect to the database
+#   connection = pymysql.connect(host=db['host'],
+#                              user=db['user'],
+#                              passwd=db['pass'],
+#                              db=db['db'],
+#                              charset='utf8mb4',
+#                              cursorclass=pymysql.cursors.DictCursor)
+#   return connection
+
+# log_file = '/home/blendax/www/log/python/api.log'
+# if os.path.isfile(log_file):
+#   logger = logging.getLogger('app')
+#   handler = logging.FileHandler(log_file)
+#   formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+#   handler.setFormatter(formatter)
+#   logger.addHandler(handler)
+#   logger.setLevel(logging.DEBUG)
--- app/main.py
+++ app/main.py
@@ -6,33 +6,19 @@ from .config import Config
 #from app.models.stamp import Stamp
 #from app.controllers.logger import Log as Logger
 
-import pymysql.cursors
-import configparser
 import logging
 import os
 import sys
 
 @app.route('/')
 def index():
-  response = json.dumps({'hello': 'world'})
-  return set_access_control(response, 'application/json'), 200
+  return render_template('index.html')
+  # return set_access_control(response, 'application/json'), 200
 
 @app.before_request
 def before_request():
   c = Config()
-  print(c.log())
-  pass
-  # log_file = '/home/blendax/www/log/python/api.log'
-  # if os.path.isfile(log_file):
-  #   logger = logging.getLogger('app')
-  #   handler = logging.FileHandler(log_file)
-  #   formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-  #   handler.setFormatter(formatter)
-  #   logger.addHandler(handler)
-  #   logger.setLevel(logging.DEBUG)
-
-
-
+  print(c.log('hello', 'world'))
 
 # HELPER FUNCTIONS #############################################################
 
@@ -40,27 +26,6 @@ def set_access_control(msg, mimetype):
   resp = Response(msg, mimetype=mimetype)
   resp.headers['Access-Control-Allow-Origin'] = '*'
   return resp
-
-def get_connection():
-  db = get_db_config()
-  # Connect to the database
-  connection = pymysql.connect(host=db['host'],
-                             user=db['user'],
-                             passwd=db['pass'],
-                             db=db['db'],
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-  return connection
-
-def get_db_config():
-  config = get_config()
-  return config['andromeda']
-
-def get_config():
-  config = configparser.ConfigParser()
-  config.read('/home/blendax/www/lib/digitalocean.ini')
-
-  return config
 
 def unauthorized():
   response = json.dumps({'message': 'Not Authorized'})
