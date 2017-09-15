6c5435aefb33173ea9d25e374adec22e20e8b196
Eric Vintimilla <eric@vintimilla.org>
2017-09-15 17:28:05 -0400

    removed bash files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# The bundle.js file will be served from memory by the development server.
-# For production builds, you'll want to use npm run build to build an optimized bundle.js,
-# which will be saved to the filesystem.
-
-npm run build
--- dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "http://localhost:8080/app/templates/dev.html"
-npm run dev
