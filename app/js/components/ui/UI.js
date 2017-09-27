9054d96e6ffbe9d2d2689771ae91f53399a3f175
Eric Vintimilla <eric@vintimilla.org>
2017-09-27 13:27:27 -0400

    resized layout

--- app/js/components/ui/UI.js
+++ app/js/components/ui/UI.js
@@ -6,27 +6,16 @@ import BottomPanel from './BottomPanel';
 class UI extends Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   elems: topElems,
-    //   right: { body: this.props.store.getState().User.user_name }
-    // };
-
-    // props.store.subscribe(() => {
-    //   this.setState({
-    //     right: { body: this.props.store.getState().User.user_name }
-    //   });
-    // });
   }
 
   render() {
 
     return (
       <div className="">
-        <div className="col-lg-8 col-md-8 col-sm-8"><MainPanel store={this.props.store}/></div>
-        <div className=""><p></p></div>
-        <div className="col-lg-4 col-md-4 col-sm-4"><RightPanel store={this.props.store}/></div>
-        <div className="col-lg-12 col-md-12 col-sm-12">
+        <div className="col-lg-8 col-md-8 col-sm-8 col-xs-12"><MainPanel store={this.props.store}/></div>
+
+        <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12"><RightPanel store={this.props.store}/></div>
+        <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <BottomPanel store={this.props.store}/>
         </div>
       </div>
